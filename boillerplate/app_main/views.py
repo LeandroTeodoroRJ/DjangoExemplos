b0a7c9ed64b370bb5e5656a72173873ec8bf9697
LeandroTeodoroRJ <leandroteodoro.rj@gmail.com>
2022-05-07 18:52:28 -0300

    Atz boillerplate

--- boillerplate/app_main/views.py
+++ boillerplate/app_main/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'app_main/home.html')     #Função para chamada de um arquivo html, note o caminho
-                                                     #que tem como base a pasta template.
+    return render(request, 'app_main/pages/home.html')     #Função para chamada de um arquivo html, note o caminho
+                                                           #que tem como base a pasta template.
 

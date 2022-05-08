35b597c0843d7cea59425f7f38f4e82dffc0dd71
LeandroTeodoroRJ <leandroteodoro.rj@gmail.com>
2022-05-08 19:00:19 -0300

    Add Exemplo Arquivos Estáticos

---
+++ arquivos_estaticos/app_main/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'app_main/pages/home.html')     #Função para chamada de um arquivo html, note o caminho
+                                                           #que tem como base a pasta template.
+

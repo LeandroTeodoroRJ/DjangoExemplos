596d0256fd6925dafd84f5849b0d0ea1af0c4516
LeandroTeodoroRJ <leandroteodoro.rj@gmail.com>
2024-05-06 15:06:03 -0300

    Update

--- blog/app_main/views.py
+++ blog/app_main/views.py
@@ -1,99 +1,34 @@
 from django.shortcuts import render
-from datetime import date
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.template.loader import render_to_string
+from .models import Post
 
-
-all_posts = [
-    {
-        "slug": "hike-in-the-mountains",
-        "image": "mountain1.jpeg",
-        "author": "Maximilian",
-        "date": date(2021, 7, 21),
-        "title": "Mountain Hiking",
-        "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "programming-is-fun",
-        "image": "mountain2.jpeg",
-        "author": "Maximilian",
-        "date": date(2022, 3, 10),
-        "title": "Programming Is Great!",
-        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "into-the-woods",
-        "image": "nature.jpeg",
-        "author": "Maximilian",
-        "date": date(2020, 8, 5),
-        "title": "Nature At Its Best",
-        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    }
-]
-
-def get_date(posts):
-    return posts['date']
-
-############################################################################
 
 # Create your views here.
-#def home(request):
-#    return render(request, 'app_main/pages/home.html')     #Função para chamada de um arquivo html, note o caminho
-                                                           #que tem como base a pasta template.
 
 
 def starting_page(request):
-    sorted_posts = sorted(all_posts, key=get_date)
-    latest_posts = sorted_posts[-3:]
+    #Ordena pela data de forma descendente e retorna os 3 primeiros
+    latest_posts = Post.objects.all().order_by("-date")[:3]
     return render(request, 'app_main/pages/index.html', {
         "posts": latest_posts
     })
 
 
 def posts(request):
+    all_posts = Post.objects.all().order_by("-date")
     return render(request, 'app_main/pages/all-posts.html', {
         "all_posts_template": all_posts
     })
 
 def post_detail(request, slug):
-    single_post = next(i for i in all_posts if i['slug'] == slug)
-    return render(request, 'app_main/pages/post-detail.html', {
-        "single_post_template": single_post
-    })
+    try:
+        single_post = Post.objects.get(slug=slug)
+        return render(request, 'app_main/pages/post-detail.html', {
+            "single_post_template": single_post,
+            "post_tags": single_post.tags.all()  #Retorna a lista de tags
+            })
+    except:
+        #Return page not found - 404 error
+        response_data = render_to_string("global/404.html")
+        return HttpResponseNotFound(response_data)

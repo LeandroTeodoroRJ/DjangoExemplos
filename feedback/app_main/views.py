1c6353cf44ce3427831a363b299acbce0791b9cb
LeandroTeodoroRJ <leandroteodoro.rj@gmail.com>
2024-05-10 22:13:45 -0300

    Update

--- feedback/app_main/views.py
+++ feedback/app_main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 #from django.http import HttpResponseRedirect
 
 from .forms import ReviewForm
+from .models import ReviewModel
 
 # Create your views here.
 
@@ -9,7 +10,19 @@ def home(request):
     if (request.method == "POST"):       #After send the form
         form = ReviewForm(request.POST)
         if (form.is_valid()):
-            print(form.cleaned_data)     #Return a dictionary with data
+            #print(form.cleaned_data)     #Return a dictionary with data
+
+            #Saving a simple Form
+            #review = ReviewModel(
+            #    user_name = form.cleaned_data['user_name'],
+            #    review_text = form.cleaned_data['review_text'],
+            #    rating = form.cleaned_data['rating'],
+            #)
+            #review.save()
+
+            #Saving a model form
+            form.save()
+
             return render(request, 'app_main/pages/thank_you.html')
 
     else:   #GET request
